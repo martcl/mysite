b14f62ca4678bbb7d8e439b7354a0eb0512a9f51
Martin Clementz <martin.hosle@gmail.com>
2021-09-04 14:03:49 +0200

    test css

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   plugins: [
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-image",
+    "gatsby-plugin-styled-components",
     "gatsby-plugin-sharp",
     {
       resolve: `gatsby-source-filesystem`,
